b18019fd63a1885804fbf29409296ee17eac5b0f
Claire Le Goues <clegoues@cs.cmu.edu>
2024-02-08 15:44:08 +0000

    These scripts are a lie now

--- PFL_Fixer.sh
+++
@@ -1,7 +0,0 @@
-
-proj=Time
-for id in $(seq 1 27)
-do
-    java -Xmx4g -cp "target/dependency/*" edu.lu.uni.serval.tbar.main.MainPerfectFL ${proj}_${id}
-done
-
--- PerfectFLTBarRunner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-bugDataPath=$1
-bugID=$2
-defects4jHome=$3
-isTestFixPatterns=$4
-
-java -Xmx1g -cp "target/dependency/*" edu.lu.uni.serval.tbar.main.MainPerfectFL $bugDataPath $bugID $defects4jHome $isTestFixPatterns
